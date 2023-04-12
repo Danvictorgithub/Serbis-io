a354a08b1cc41ce7bcb9e925e2c96dbfa3bafa2e
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-04-12 20:52:54 +0800

    improved inbox component

--- src/views/footer/footer.tsx
+++ src/views/footer/footer.tsx
@@ -4,11 +4,12 @@ import HomeLogo from "../../assets/homeLogo";
 import MapLogo from "../../assets/mapLogo";
 import React from 'react';
 import { Link } from "react-router-dom";
+
 export default function Footer() {
 	return (
 		<div className='footer'>
 			<div className='navBot'>
-				<Link to="/"><HomeLogo/></Link>
+				<Link to="/"><HomeLogo /></Link>
 				<Link to="/map"><MapLogo/></Link>
 				<Link to="/inbox"><EmailLogo/></Link>
 				<Link to="/profile"><AccountLogo/></Link>
--- src/views/mail/mail.tsx
+++ src/views/mail/mail.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
-
+import uniqid from "uniqid";
 interface Mail {
+	user:string,
 	message:string
 }
 
 export default function Mail() {
 	const mailArray:Mail[] = [
-		{message:"lorem ipsum es camat lorem ipsum es camatlorem ipsum es camatlorem ipsum es camatlorem ipsum es camat"},
-		{message:"lorem ipsum es camat lorem ipsum es camatlorem ipsum es camatlorem ipsum es camatlorem ipsum es camat"}
+		{message:"lorem ipsum es camat lorem ipsum es camatlorem ipsum es camatlorem ipsum es camatlorem ipsum es camat",
+		 user:uniqid()
+		},
+		{message:"lorem ipsum es camat lorem ipsum es camatlorem ipsum es camatlorem ipsum es camatlorem ipsum es camat",
+		 user:uniqid()
+		}
 		];
 	const isMailEmpty = (mailArray.length == 0);
 	return (
 		<div className='mail container'>
-			{(isMailEmpty) ? <h2 className='mailEmpty'>Your mail is empty</h2>:<></>}
-			{mailArray.map((mail:Mail):JSX.Element=> {
-				return <div className="mailContainer">{mail.message}</div>
-			})}
+			<div className='wrapper'>
+				{(isMailEmpty) ? <h2 className='mailEmpty'>Your mail is empty</h2>:<></>}
+				{mailArray.map((mail:Mail):JSX.Element=> {
+					return <div className="mailContainer" key={uniqid()}>
+						<img className="defaultProfile" src="/defaultProfile.jpg"></img>
+						<h3>{mail.user}</h3>
+						<p className='mailMessage'>{mail.message}</p>											
+					</div>
+				})}
+			</div>
 		</div>
 	)
 }
