6735ccd3314958cde5e5ee0954272028bc126b42
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-04-12 23:24:48 +0800

    added profile ui

--- src/views/profile/profile.tsx
+++ src/views/profile/profile.tsx
@@ -3,7 +3,19 @@ import React from 'react';
 export default function Profile() {
 	return (
 		<div className='profile container'>
-			<h2>Please log-in to continue</h2>
+			<div className='wrapper'>
+				<img className="profile-landing-image"src="profile-landing-image.png"></img>
+				<form className='login-auth'>
+					<h3>Login to get your favorite services</h3>
+					<div className='inputGroup'>
+						<label htmlFor='username' hidden>Username</label>
+						<input type="text" maxLength={32} placeholder='Username' name="username"></input>
+						<label htmlFor='username' hidden>Password</label>
+						<input type="password" maxLength={32} placeholder='Password' name="password"></input>
+						<button className='loginSubmit' type='button'>Login</button>
+					</div>
+				</form>
+			</div>
 		</div>
 	)
 }
