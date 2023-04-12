34f8a29387fb87d76efccc0f2e4306aace343b33
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-04-13 00:37:48 +0800

    fixed production issue

--- src/views/map/components/leaflet-map.tsx
+++ src/views/map/components/leaflet-map.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import "leaflet/dist/leaflet.css"
-import { MapContainer } from 'react-leaflet/MapContainer'
-import { TileLayer } from 'react-leaflet/TileLayer'
-import { useMap } from 'react-leaflet/hooks';
-import { Marker } from 'react-leaflet/Marker';
-import { Popup } from 'react-leaflet/Popup';
+import "leaflet/dist/leaflet.css";
+import {MapContainer,TileLayer,useMap} from 'react-leaflet';
 function MyComponent() {
   const map = useMap()
   console.log('map center:', map.getCenter())
@@ -13,10 +9,10 @@ function MyComponent() {
 export default function LeafletMap() {
 	return (
 		<MapContainer center={[8.9475377, 125.54062339999996]} zoom={13} scrollWheelZoom={true}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-</MapContainer>
+		  <TileLayer
+		    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+		  />
+		</MapContainer>
 	)
 }
