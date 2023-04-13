03508ce945b143751a235c3b8e447f412190bf5d
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-04-13 15:58:08 +0800

    Added get user location feature for maps

--- src/views/map/components/leaflet-map.tsx
+++ src/views/map/components/leaflet-map.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "leaflet/dist/leaflet.css";
-import {MapContainer,TileLayer,useMap} from 'react-leaflet';
-function MyComponent() {
-  const map = useMap()
-  console.log('map center:', map.getCenter())
-  return null
+import {MapContainer,TileLayer,useMap ,useMapEvents,useMapEvent, Marker, Popup} from 'react-leaflet';
+interface LatLngLiteral {
+    lat: number;
+    lng: number;
+}
+function SetUserLocation():JSX.Element {
+  const [position, setPosition] = useState<null| LatLngLiteral>(null)
+  const map = useMapEvents({
+      dblclick() {
+        map.locate();
+      },
+      locationfound(e) {
+        setPosition(e.latlng)
+        map.flyTo(e.latlng,map.getZoom());
+      }
+    });
+  return position === null ? <></> : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
+function WatchCenter():JSX.Element {
+  const map = useMapEvent('moveend',()=> {
+    console.log(map.getCenter());
+  });
+  return (<></>);
 }
 export default function LeafletMap() {
 	return (
-		<MapContainer center={[8.9475377, 125.54062339999996]} zoom={13} scrollWheelZoom={true}>
+		<MapContainer center={[8.9475377, 125.54062339999996]} zoom={13} scrollWheelZoom={true} doubleClickZoom={false}>
 		  <TileLayer
-		    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		    attribution='&copy; <a href="https://	www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 		    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 		  />
+		  <SetUserLocation/>
+      <WatchCenter/>
 		</MapContainer>
 	)
 }
