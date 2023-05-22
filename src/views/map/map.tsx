68ae2b490ac9b7a4aff28186ad554e97aabacd6f
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-05-22 23:53:12 +0800

    removed unnecessary elements

--- src/views/map/map.tsx
+++ src/views/map/map.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import LeafletMap from "./components/leaflet-map";
 import LocationModal from './components/locationModal';
 import MapContext from "./components/utils/mapContext";
-export default function Map() {
+export default function Map():JSX.Element {
 	const [currentLocation,setCurrentLocation] = useState<null|string>(null);
 	return (
 		<div className='map container'>
